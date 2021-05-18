0b357044d006bcfeb0d54ce2899a0756310d1649
krishii3 <mridulkrishn3@gmail.com>
2021-05-18 16:58:30 +0530

    fixed bugs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app= Flask(__name__)
 
@@ -6,6 +6,13 @@ app= Flask(__name__)
 
 def index():
     return render_template('index.html')
+
+@app.route('/get_data')
+
+def getAPI():
+    data= request.data
+    print(data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
