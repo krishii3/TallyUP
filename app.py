fafc39f28fa54750b738cf2b4762877bbfecfe8a
krishii3 <mridulkrishn3@gmail.com>
2021-05-18 17:40:40 +0530

    base_game

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask,render_template,request,jsonify
-
+from flask import Flask,render_template,request
+import json
 app= Flask(__name__)
+
+
+
+def algo(data):
+    pass
+    
+
+
+
+
+
+
+
+
+
 
 @app.route('/')
 
@@ -9,10 +24,13 @@ def index():
 
 @app.route('/get_data', methods=["POST","GET"])
 def getAPI():
-    data= request.data
-    print(data)
+    data= json.loads(request.data)
+    algo(data)
     return {"success":"1"}
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
